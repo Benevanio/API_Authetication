427e67c4d9c727fef3756bbf316dd97028838e11
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-03 16:51:52 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const  exp
+const express = require('express');
+const app = express();
+
+const port = 3008;
+
+const authRoute = require('./routes/auth');
+
+app.use('/api/user', authRoute);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
