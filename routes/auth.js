32c6f70129477b2b50fdc702569883548ce36dd6
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-03 20:01:02 -0300

    Resolve validation schema

--- routes/auth.js
+++ routes/auth.js
@@ -18,10 +18,9 @@ const schema = Joi.object({
 });
 router.post('/register', async (req, res) => {
     // Validate the data before we make a user
-    const validation =  schema.validate(req.body)
-    res.send(validation)
-    
-    /*const user = new User({
+    const { error } = schema.validate(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
+    const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
@@ -33,7 +32,7 @@ router.post('/register', async (req, res) => {
     }catch(err){
         res .status(400).send(err);
     }
-    */
+   
 });
 
 module.exports = router;
