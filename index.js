c1847f006c5b87e37e8c88f691ad898d6c80b121
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-03 17:49:30 -0300

    dotenv:add security for my app conection

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(
+    
+    process.env.CONNECT,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},()=>
+console.log('Connected to DB!'))
 
 const port = 3008;
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 router.post('/register', (req, res) => {
-    res.send('Register');
+    res.json({
+       name: "Bene",
+         email: "Bene@gmail.com",
+         passsword: "123456",
+         age: 25
+
+    }); 
 });
 
 module.exports = router;
