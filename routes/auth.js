5f1cf18acba8344263f8bc0a758dcddde14c5d7a
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-03 21:06:27 -0300

    verify token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const Jwt = require('jsonwebtoken');
 const User = require('../model/user');
 const {
     RegisterValidation
@@ -17,24 +18,51 @@ router.post('/register', async (req, res) => {
     });
     if (emailExist) return res.status(400).send('Email already exists');
 
-const salt = await bcrypt.genSalt(10);
-const Haspassword = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const Haspassword = await bcrypt.hash(req.body.password, salt);
 
 
-const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: Haspassword,
-    date: req.body.date
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: Haspassword,
+        date: req.body.date
+    });
+    try {
+        //save user to MongoDB
+        const savedUser = await user.save();
+        res.send({
+            user: user._id
+        });
+    } catch (err) {
+        //res.status in case of error
+        res.status(400).send(err);
+    }
 });
-try {
-    //save user to MongoDB
-    const savedUser = await user.save();
-    res.send(savedUser);
-} catch (err) {
-    //res.status in case of error
-    res.status(400).send(err);
-}
+
+//Loginb
+router.post('/login', async (req, res) => {
+    // Validate the data before we make a user
+    const {
+        error
+    } = RegisterValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    //email exist
+    const emailExist = await User.findOne({
+        email: req.body.email
+    });
+    if (emailExist) return res.status(400).send('Email not found');
+    //password is correct
+    const validPass = await bcrypt.compare(req.body.password, User.password);
+    if (!validPass) return res.status(400).send('Invalid password');
+
+    const token = Jwt.sign({
+            _id: User._id
+        },
+        process.env.Token_Secret)
+    res.header('auth-token', token).send(token);
 });
+
+
 
 module.exports = router;
---
+++ routes/verify.js
@@ -0,0 +1,14 @@
+const Jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).send('Access Denied');
+
+    try {
+        const verified = Jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send('Invalid Token');
+    }
+}
